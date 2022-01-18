218709a4f332d5c5c154e92a5975fc0bb950ee1e
Carlos Gonzalez <carlos.gonzalez@datadoghq.com>
2022-01-18 15:32:13 +0000

    Try imitate circleCI

---
+++ .github/actions/datadog-agent/index.js
@@ -0,0 +1,34 @@
+const core = require('@actions/core');
+const { spawnSync } = require('child_process');
+
+core.info('Starting docker Datadog agent')
+
+const dockerPull = spawnSync('docker', [
+    'pull',
+     core.getInput('registryPath', { required: true }),
+    ],{ stdio: 'inherit' });
+if (dockerPull.error || dockerPull.status !== 0) {
+    core.setFailed('could not pull docker image');
+    process.exit(1)
+}
+
+core.info('Running agent');
+const dockerRun = spawnSync('docker', [
+    'run',
+    '-d',
+    '--name', core.getInput('containerName', { required: true }),
+    '-v', '/var/run/docker.sock:/var/run/docker.sock:ro',
+    '-v', 'proc/:/host/proc/:ro',
+    '-v', '/sys/fs/cgroup/:/host/sys/fs/cgroup:ro',
+    '-e',  `DD_API_KEY=${core.getInput('ApiKey', { required: true })}`,
+    '-e', 'DD_INSIDE_CI=true',
+    '-e', 'DD_HOSTNAME=none',
+    '-e', `DD_SITE=${core.getInput('site', { required: true })}`
+    ], { stdio: 'inherit' });
+
+if (dockerRun.error || dockerRun.status !== 0) {
+    core.setFailed('could not run docker image');
+    process.exit(1)
+}
+
+core.info('Started docker agent');
--- .github/actions/stop-agent/index.js
+++
@@ -1,2 +0,0 @@
-const core = require('@actions/core');
-core.info('Will stop the agent at the end');
