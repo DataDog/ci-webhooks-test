c5c339edffd9281d31b69d1c79a080ae30b2d082
Carlos Gonzalez <carlos.gonzalez@datadoghq.com>
2022-01-18 13:01:24 +0000

    Update stop.js

--- .github/actions/stop-agent/stop.js
+++ .github/actions/stop-agent/stop.js
@@ -1,4 +1,5 @@
 const { spawnSync } = require('child_process');
+const core = require('@actions/core');
 
 core.info('Stopping datadog agent in docker container');
 const child = spawnSync(`docker exec -t ${core.getInput('containerName', { required: true })} agent stop`, timeout=300000);
