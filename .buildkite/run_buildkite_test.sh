baa47ba447b07897d844a8f8468ad16864b66515
Carlos Gonzalez <carlos.gonzalez@datadoghq.com>
2022-02-04 09:05:49 +0000

    Update buildkite pipeline for experiment

---
+++ .buildkite/run_buildkite_test.sh
@@ -0,0 +1,14 @@
+echo "Clearing test cache"
+go clean -testcache
+
+echo "Running Go tests"
+gotestsum --format testname --junitfile test.xml
+
+exit_code=$?
+
+set -e
+echo "Uploading XML to Data Dog"
+DATADOG_API_KEY=datadog-ci junit upload --service carlos-test test.xml
+
+echo "Exiting with ${exit_code}"
+exit ${exit_code}
