fd7e945afe9a18184e463acef6dcc8aa5efca68a
Carlos Gonzalez <carlos.gonzalez@datadoghq.com>
2022-02-18 13:36:56 +0000

    Update run_buildkite_test.sh

--- .buildkite/run_buildkite_test.sh
+++ .buildkite/run_buildkite_test.sh
@@ -1,3 +1,5 @@
+!#/bin/bash
+
 echo "Clearing test cache"
 go clean -testcache
 
@@ -10,6 +12,7 @@ set -e
 echo "Uploading XML to Data Dog"
 datadog-ci junit upload --service carlos-test test.xml
 
+set -x
 echo `ls -l test.xml`
 echo "Exiting with ${exit_code}"
 exit ${exit_code}
