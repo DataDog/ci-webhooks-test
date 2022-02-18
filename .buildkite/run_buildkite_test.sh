d127401d98ece7f3087d403a71f6bcb486cf7da1
Carlos Gonzalez <carlos.gonzalez@datadoghq.com>
2022-02-18 10:54:21 +0000

    Add junit size to script

--- .buildkite/run_buildkite_test.sh
+++ .buildkite/run_buildkite_test.sh
@@ -10,5 +10,7 @@ set -e
 echo "Uploading XML to Data Dog"
 datadog-ci junit upload --service carlos-test test.xml
 
+ls -l test.xml | awk '{print $4}' | tr -d '\n' | buildkite-agent meta-data set "dd_metrics.junit.size"
+
 echo "Exiting with ${exit_code}"
 exit ${exit_code}
