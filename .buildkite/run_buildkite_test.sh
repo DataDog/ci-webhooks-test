34e556e4e918cb4e07728e8a01aa6a3bd1c31e0a
Carlos Gonzalez <carlos.gonzalez@datadoghq.com>
2022-02-18 13:54:35 +0000

    Update run_buildkite_test.sh

--- .buildkite/run_buildkite_test.sh
+++ .buildkite/run_buildkite_test.sh
@@ -12,7 +12,10 @@ set -e
 echo "Uploading XML to Data Dog"
 datadog-ci junit upload --service carlos-test test.xml
 
-ls -l test.xml | awk '{print $5}' | tr -d '\n' | buildkite-agent meta-data set "dd_metrics.junit.size_bytes
+echo "$(ls -l test.xml | awk '{print $5}'"
+size=$(ls -l test.xml | awk '{print $5}' | tr -d '\n')
+
+echo "$size"
 
 echo "Exiting with ${exit_code}"
 exit ${exit_code}
