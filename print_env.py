8e0afae8b4a37a65ff056a02cedd270da01fe09f
Carlos Gonzalez <carlos.gonzalez@datadoghq.com>
2022-05-30 10:01:11 +0100

    Get GH env vars

---
+++ print_env.py
@@ -0,0 +1,10 @@
+import re
+import os
+
+
+envs = []
+for k, v in os.environ.items():
+    if k.startswith("GITHUB_") and not re.search('(PASS)|(TOKEN)|(SECRET)|(KEY)', k):
+        envs.append(f'{k}={v}')
+
+print(f'ci.env_vars:{",".join(envs)}')
