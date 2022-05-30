aa25753fa4cd3d587c9bbe23f84427222a5489a3
Carlos Gonzalez <carlos.gonzalez@datadoghq.com>
2022-05-30 10:04:52 +0100

    Update print_env.py

--- print_env.py
+++ print_env.py
@@ -4,7 +4,7 @@ import os
 
 envs = []
 for k, v in os.environ.items():
-    if k.startswith("GITHUB_") and not re.search('(PASS)|(TOKEN)|(SECRET)|(KEY)', k):
+    if k.startswith("GITHUB_") and not re.search('(PASS)|(TOKEN)|(SECRET)|(KEY)', k) and v:
         envs.append(f'{k}={v}')
 
 print(f'ci.env_vars:{",".join(envs)}')
