87eaffadbdd33d7ba270369d035eb816855c4455
Carlos Gonzalez <carlos.gonzalez@datadoghq.com>
2022-05-30 11:19:53 +0100

    Update print_env.py

--- print_env.py
+++ print_env.py
@@ -8,4 +8,4 @@ for k, v in os.environ.items():
     if k.startswith("GITHUB_") and not re.search('(PASS)|(TOKEN)|(SECRET)|(KEY)', k) and v:
         envs[k] = v
 
-print(f'ci.env_vars:{json.dumps(envs, sort_keys=True, separators=(',', ':'))}')
+print(f'ci.env_vars:{json.dumps(envs, sort_keys=True, separators=(",", ":"))}')
