862e699244d0daff740cd659e3f2aad803a04eb5
Carlos Gonzalez <carlos.gonzalez@datadoghq.com>
2022-08-12 12:01:12 +0100

    [buildkite] more tests

--- python_tests/sample_test.py
+++ python_tests/sample_test.py
@@ -1,6 +1,9 @@
 from unittest import TestCase
 from random import randrange
+from exceptions import ValueError
 
+def my_func():
+    raise ValueError
 
 class TestTrivial(TestCase):
     def test_pass(self):
@@ -11,3 +14,7 @@ class TestTrivial(TestCase):
     
     def test_fail(self):
         assert 5 == 7
+    
+    def test_1(self):
+        assert my_func() == 4
+   
